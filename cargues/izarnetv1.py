9d9d9194cba0d87988251f01f1b00a6cd80d0969
francisco <fjmoralesp@outlook.com>
2016-08-16 22:13:53 -0500

    Cargue izarnetv1 - Ingresar registros a BD

--- cargues/izarnetv1.py
+++ cargues/izarnetv1.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
 import MySQLdb
 from datetime import datetime
+import glob
 
 conn = MySQLdb.connect(host='localhost',
                   user='root',
@@ -13,33 +13,41 @@ cursor = conn.cursor()
 def CargueExcel(archivo):
 	return pd.read_excel(archivo)
 
-def CargueRegistros(data):
+def CargueRegistros(data, file_name):
 	headers = []
+	file_name = file_name.replace('Info', '')
+	file_name = file_name.replace('_Pre.xls', '')
 	add_partial = ('INSERT INTO izarnetv1_izarnetv1 '
-		'(fecha, volumen, consumo, volumen_litros, caudal, alarma) ')
-
+		'(fecha, volumen, consumo, volumen_litros, caudal, alarma, medidor_id) ')
+	get_medidor = ('SELECT id FROM medidores_medidor '
+		'WHERE serial = "{}"'.format(file_name))
+	cursor.execute(get_medidor)
+	medidor_id = cursor.fetchone()
+	medidor_id = medidor_id[0]
 	for header in list(data.columns.values):
 		headers.append(header)
 
 	for row in data.iterrows():
 		fecha = row[1][headers[0]]
+		fecha = datetime.strptime(fecha, '%d/%m/%y %I:%M %p')
 		volumen = row[1][headers[1]]
 		consumo = row[1][headers[2]]
 		volumen_litros = volumen*1000
 		caudal = volumen_litros*60
 		alarma = row[1][headers[5]]
-
-		datetime.strptime(fecha, '%d/%m/%Y %H:%M')
-		print fecha
-
-		add_row = add_partial + ('VALUES ({}, {}, {}, {}, {}, {})'.format(
-			fecha, volumen, consumo, volumen_litros, caudal, alarma))
-		print add_row
-		#cursor.execute(add_row)
+		alarma = alarma.encode('ascii', 'ignore')
+		add_row = add_partial + ('VALUES ("{}", {}, {}, {}, {}, "{}", {})'.
+			format(fecha, volumen, consumo, volumen_litros, 
+				caudal, alarma, medidor_id))
+		cursor.execute(add_row)
 
 	conn.commit()
 	cursor.close()
 	conn.close()
+	print 'Se han cargado todos los datos'
 
-data = CargueExcel('InfoC11LA004183_Pre.xls')
-CargueRegistros(data)
+path = ''
+file_names = glob.glob(path + '*.xls')
+for file in file_names:
+	data = CargueExcel(file)
+	CargueRegistros(data, file)
