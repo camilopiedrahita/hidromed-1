6e38f97243cd7a5775c1955775f9532174117ef9
francisco <fjmoralesp@outlook.com>
2016-08-24 23:18:33 -0500

    Ajuste admin site IzarnetV1 e IzarnetV2

--- hidromed/izarnetv1/admin.py
+++ hidromed/izarnetv1/admin.py
@@ -4,6 +4,7 @@ from .models import *
 
 @admin.register(Izarnetv1)
 class AdminIzarnetv1(admin.ModelAdmin):
+	list_filter = ('medidor',)
 	list_display = ('id', 'medidor', 'fecha', 'volumen',
 		'consumo', 'volumen_litros', 'caudal', 'alarma',)
 
--- hidromed/izarnetv2/admin.py
+++ hidromed/izarnetv2/admin.py
@@ -4,6 +4,7 @@ from .models import *
 
 @admin.register(Izarnetv2)
 class AdminIzarnetv2(admin.ModelAdmin):
+	list_filter = ('medidor',)
 	list_display = ('id', 'medidor', 'fecha', 'volumen_litros',
 		'consumo', 'caudal', 'alarma',)
 
