d4ad5c0dbbd4fe6ba6ecada70e2a1559060c86cb
francisco <fjmoralesp@outlook.com>
2016-10-08 20:56:23 -0500

    Recalcular al cargar datos antiguos

--- cargues/izarnetv2.py
+++ cargues/izarnetv2.py
@@ -20,6 +20,43 @@ def Log(mensaje):
 	file_log.write(mensaje + '\n')
 	file_log.close()
 
+def RecalcData(id_desde, id_hasta, last):
+	data_partial = ('SELECT id, caudal, consumo, consumo_acumulado, fecha FROM izarnet_izarnet '
+		'WHERE id >= {} AND id < {};'.format(
+		id_desde, id_hasta))
+	initial_partial = ('SELECT id, caudal, consumo, consumo_acumulado, fecha FROM izarnet_izarnet '
+		'WHERE fecha = "{}";'.format(last))
+	cursor.execute(data_partial)
+	data = cursor.fetchall()
+	cursor.execute(initial_partial)
+	initial = cursor.fetchone()
+	last_consumo_acumulado = initial[3]
+	last_fecha = initial[4]
+
+	for registro in data:
+		id = registro[0]
+		new_caudal = registro[1]
+		new_consumo = registro[2]
+		new_consumo_acumulado = registro[3]
+		new_fecha = registro[4]
+		if (new_fecha - last_fecha).total_seconds() < 0:
+			minutos = (((new_fecha - last_fecha)*-1).total_seconds())/60
+		else:
+			minutos = ((new_fecha - last_fecha).total_seconds())/60
+		caudal = new_consumo / minutos * 60
+		consumo_acumulado = last_consumo_acumulado + new_consumo
+		udpate = ('UPDATE izarnet_izarnet SET '
+			'caudal = {}, ' 
+			'consumo_acumulado = {} '
+			'WHERE id = {};'.format(
+				caudal,
+				consumo_acumulado,
+				id))
+		cursor.execute(udpate)
+		last_consumo_acumulado = consumo_acumulado
+		last_fecha = new_fecha
+	conn.commit()
+
 def CargueRegistros(data, file_name):
 	#Queries partials
 	add_procesados_partial = ('INSERT INTO izarnet_izarnetprocesados '
@@ -31,8 +68,11 @@ def CargueRegistros(data, file_name):
 	last_fecha_partial = ('SELECT fecha FROM izarnet_izarnet ')
 	last_consumo_acumulado_partial = ('SELECT consumo_acumulado FROM izarnet_izarnet ')
 	id_match_partial = ('SELECT id FROM izarnet_izarnet ')
+	min_id_partial = ('SELECT MIN(id) FROM izarnet_izarnet ')
+	id_hasta_partial = ('SELECT id FROM izarnet_izarnet ')
 
 	headers = []
+	recalcular = False
 	estado = 'Cargue correcto'
 	parsed_file_name = file_name.split('_')[1]
 	get_medidor = get_medidor_partial + (
@@ -56,6 +96,15 @@ def CargueRegistros(data, file_name):
 			volumen = volumen_litros/1000
 			alarma = u'%s' % row[1][headers[4]]
 			alarma = alarma.encode('ascii', 'ignore')
+			min_id = min_id_partial + (
+				'WHERE medidor_id = "{}" AND fecha > "{}"'.format(
+					medidor_id, str(fecha)))
+			cursor.execute(min_id)
+			min_id_data = cursor.fetchone()
+			if not min_id_data == None and recalcular == False:
+				fecha_hasta = fecha
+				id_found = False
+				recalcular = True
 			last_id = last_id_partial + (
 				'WHERE medidor_id = "{}" AND fecha < "{}"'.format(
 					medidor_id, str(fecha)))
@@ -92,6 +141,7 @@ def CargueRegistros(data, file_name):
 				add_row = add_partial + ('VALUES ("{}", {}, {}, {}, {}, "{}", {}, {})'.
 					format(fecha, volumen, consumo, volumen_litros, 
 						caudal, alarma, medidor_id, consumo_acumulado))
+				last = fecha
 			else:
 				add_row = ('UPDATE izarnet_izarnet SET '
 					'fecha = "{}", ' 
@@ -112,8 +162,31 @@ def CargueRegistros(data, file_name):
 						medidor_id,
 						consumo_acumulado,
 						id_match_data))
+				last = fecha
 			try:
 				cursor.execute(add_row)
+				if id_found == False:
+					id_found = True
+					id_hasta = id_hasta_partial + (
+						'WHERE '
+						'fecha = "{}" AND '
+						'volumen = {} AND '
+						'consumo = {} AND '
+						'volumen_litros = {} AND '
+						'caudal = {} AND '
+						'alarma = "{}" AND '
+						'medidor_id = {} AND '
+						'consumo_acumulado = {};'.format(
+							fecha,
+							volumen,
+							consumo,
+							volumen_litros,
+							caudal,
+							alarma,
+							medidor_id,
+							consumo_acumulado))
+					cursor.execute(id_hasta)
+					id_hasta_data = cursor.fetchone()
 			except Exception, e:
 				Log(str(e))
 				Log('Error en {}'.format(add_row))
@@ -124,13 +197,16 @@ def CargueRegistros(data, file_name):
 			Log('Error en Headers del archivo Excel')
 			Log('Error en archivo {}'.format(parsed_file_name))
 			estado = 'Cargue con errores'
-	
 	add_procesados = add_procesados_partial + ('VALUES ("{}", "{}", "{}")'.
 		format(file_name, 
 			datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 
 			estado))
 	cursor.execute(add_procesados)
 	conn.commit()
+	if recalcular == True:
+		min_id_data = min_id_data[0]
+		id_hasta_data = id_hasta_data[0]
+		RecalcData(min_id_data, id_hasta_data, last)
 	print 'Se han cargado todos los datos'
 		
 path = ''
@@ -144,7 +220,7 @@ for file in file_names:
 		format='%d-%m-%Y %H:%M:%S')
 	data = data.sort_values('event.timestamp')
 	CargueRegistros(data, file)
-	#shutil.move(file, 'Procesados/' + file)
+	shutil.move(file, 'Procesados/' + file)
 
 cursor.close()
 conn.close()
